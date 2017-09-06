745b2fe6322a9620ab80b4001e5fc0dc2b956174
Shivali Sharma <shivalisharma@ucreate.co.in>
2017-09-06 16:02:16 +0530

    body class change for customer page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
   <link rel="stylesheet"
   href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition  skin-blue sidebar-mini">
 @yield('content')
 @include('layouts.footer')
 @include('layouts.footermain')
