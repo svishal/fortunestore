9d69cb27589b3b24ababdcc5544a1379c123c65f
Shivali Sharma <shivalisharma@ucreate.co.in>
2017-09-19 09:52:14 +0530

    give link at top

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
      <header class="main-header">
     <!-- Logo -->
-    <a href="#" class="logo">
+    <a href="/dashboard" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>FS</span>
       <!-- logo for regular state and mobile devices -->
